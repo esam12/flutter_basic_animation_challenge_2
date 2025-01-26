a8a0d1163648e0a40503f66b717f0bf46dd32d33
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-01-26 15:41:02 +0300

    Add initial Flutter project structure and macOS/Windows configurations
    This commit initializes the basic structure for a Flutter project, including macOS and Windows configurations. It adds necessary files such as `Info.plist`, `MainFlutterWindow.swift`, and various configuration files for macOS. For Windows, it includes CMakeLists.txt files, resource files, and other necessary setup files to build and run the Flutter application on Windows. Additionally, it sets up the initial project structure with `pubspec.yaml`, `pubspec.lock`, and test files.

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.dark,
+      debugShowCheckedModeBanner: false,
+      debugShowMaterialGrid: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 200,
+            height: 200,
+            color: Colors.red,
+          ),
+          Container(
+            width: 200,
+            height: 200,
+            color: Colors.yellow,
+          ),
+        ],
+      ),
+    );
+  }
+}
